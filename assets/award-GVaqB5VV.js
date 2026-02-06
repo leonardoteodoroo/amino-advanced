a220a114c0a30866594518f4d12d642b0e732e7c
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-06 06:34:34 -0300

    Updates

--- assets/award-GVaqB5VV.js
+++ assets/award-GVaqB5VV.js
@@ -1,4 +1,4 @@
-import{c}from"./index-Czd7xobu.js";/**
+import{c}from"./index-DxBMH4qa.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/quote-kKoT_Kof.js
+++ assets/quote-kKoT_Kof.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Czd7xobu.js";/**
+import{c as a}from"./index-DxBMH4qa.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
