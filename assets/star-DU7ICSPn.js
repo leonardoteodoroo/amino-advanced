0988f9b512ed68668862357eed3b64808ed4fc3b
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-07 00:57:51 -0300

    Updates

--- assets/star-DU7ICSPn.js
+++ assets/star-DU7ICSPn.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Ci2sxlro.js";/**
+import{c as a}from"./index-D8bd9Xab.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
