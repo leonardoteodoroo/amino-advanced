7a7a503ad87801882fbe1824970a8800641b2705
leonardoteodoroo <leonardoteodoroo@users.noreply.github.com>
2026-02-08 04:49:27 +0000

    deploy: 3efafbed36b3dafe26d9e981a5d6d46093cf92c3

--- assets/star-Dj52pI6K.js
+++ assets/star-Dj52pI6K.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B512KEmS.js";/**
+import{c as a}from"./index-BVxREdYV.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
