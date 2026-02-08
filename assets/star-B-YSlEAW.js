7b6ac506ef4847d9005c137a14585cf9a29f079d
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-08 02:05:19 -0300

    Updates

--- assets/star-B-YSlEAW.js
+++ assets/star-B-YSlEAW.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BVxREdYV.js";/**
+import{c as a}from"./index-Cm0UMSNa.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
