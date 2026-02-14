c3064ecccd6678994f0e4c9a3abff32192cc95af
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-14 14:29:38 -0300

    Updates

--- assets/quote-TQ1jVR46.js
+++ assets/quote-TQ1jVR46.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DMqYfvuH.js";/**
+import{c as a}from"./index-CuMj9QpO.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
