efef9269a7e6aed9351080ffe7a01bedaef15f4c
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-14 16:03:07 -0300

    Updates

--- assets/award-D3rLpOYz.js
+++ assets/award-D3rLpOYz.js
@@ -1,4 +1,4 @@
-import{c}from"./index-DWvHHnnv.js";/**
+import{c}from"./index-BWTc081X.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/quote-Cm7NyPaB.js
+++ assets/quote-Cm7NyPaB.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DWvHHnnv.js";/**
+import{c as a}from"./index-BWTc081X.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
