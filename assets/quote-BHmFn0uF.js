52a9a6f50e48ee593c420ee6cfd290a43077b53c
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-17 01:11:16 -0300

    Updates

--- assets/quote-BHmFn0uF.js
+++ assets/quote-BHmFn0uF.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BnBTyrVW.js";/**
+import{c as a}from"./index-CEtAFwAK.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
