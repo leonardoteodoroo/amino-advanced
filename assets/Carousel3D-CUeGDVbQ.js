d6d7d8886e92924755d32ded540e0d20fdf5726c
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-17 01:16:19 -0300

    Updates

--- assets/Carousel3D-CUeGDVbQ.js
+++ assets/Carousel3D-CUeGDVbQ.js
@@ -1,4 +1,4 @@
-import{a as m,u as q,b as C,j as e,m as I,c as r,d as M}from"./vendor-framer-Df6E5229.js";import{c as h}from"./index-CEtAFwAK.js";import{Q as S}from"./quote-BHmFn0uF.js";import{S as _}from"./star-Yb1DC20D.js";import"./vendor-react-r57bcxCc.js";/**
+import{a as m,u as q,b as C,j as e,m as I,c as r,d as M}from"./vendor-framer-Df6E5229.js";import{c as h}from"./index-DpnpvSiv.js";import{Q as S}from"./quote-BQIAom43.js";import{S as _}from"./star-DQ1ZsrFZ.js";import"./vendor-react-r57bcxCc.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
