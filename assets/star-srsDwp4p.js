81187634d045349ecd94d5c7a2dc66d1fbaab026
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-19 12:39:44 -0300

    Updates

--- assets/star-srsDwp4p.js
+++ assets/star-srsDwp4p.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DpnpvSiv.js";/**
+import{c as a}from"./index-Ve86ZHyD.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
