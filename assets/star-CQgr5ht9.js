4aa113a5b53c0c2fb3d58be38c204b03599e804b
Leonardo Teodoro <lauteodor@gmail.com>
2026-02-19 13:55:54 -0300

    Updates

--- assets/star-CQgr5ht9.js
+++ assets/star-CQgr5ht9.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DKDBl-5n.js";/**
+import{c as a}from"./index-BQS_bl7K.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
